44a141f88e01447caf6d97d2e063773a9bbf7b57
robertscalzo <rscalzo@alumni.scu.edu>
2021-09-16 13:28:10 -0500

    Second commit - figured out a conflict finder. Yet to be refactored

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -16,14 +16,17 @@ test("apply conflict colors to conflicting events", () => {
             start: '2021-09-02T00:00',
             end: '2021-09-05T00:00',
             title: 'Event 1',
+            conflicted:false
         }, {
             start: '2021-09-04T00:00',
             end: '2021-09-07T00:00',
             title: 'Event 2',
+            conflicted:false
         }, {
             start: '2021-09-20T00:00',
             end: '2021-09-21T00:00',
             title: 'Event 3',
+            conflicted:false
         }
     ];
     const myEventsWithConflicts= [
@@ -31,16 +34,19 @@ test("apply conflict colors to conflicting events", () => {
             start: '2021-09-02T00:00',
             end: '2021-09-05T00:00',
             title: 'Event 1',
+            conflicted:true,
             color:"yellow"
         }, {
             start: '2021-09-04T00:00',
             end: '2021-09-07T00:00',
             title: 'Event 2',
+            conflicted:true,
             color:"yellow"
         }, {
             start: '2021-09-20T00:00',
             end: '2021-09-21T00:00',
             title: 'Event 3',
+            conflicted:false,
             color:"blue"
         }
     ];;
--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,43 @@ setOptions({
     theme: 'ios',
     themeVariant: 'light'
 });
-export const applyConflicts = (events:MbscCalendarEvent[]) : MbscCalendarEvent[] =>{
+export const applyConflicts = (events: MbscCalendarEvent[]): MbscCalendarEvent[] => {
+    function checkOverlap(events: MbscCalendarEvent[]): MbscCalendarEvent[] {
+        const overlap: MbscCalendarEvent[] = [];
+        let start: MbscCalendarEvent[] = [];
+        events.forEach((e) => {
+            let d = [e.start, e.end]
+            start.push(d);
+        });
+        start.sort();
+        start.forEach((e, i) => {
+            if (start[i + 1]) {
+                if (e[1] > start[i + 1]) {
+                    overlap.push(start[0], start[1])
+                }
+            }
+        });
+        return overlap;
+    }
 
+    const conflictedEvents = checkOverlap(events)
+    events.map((event) => {
+            conflictedEvents.map((conflictedEvent) => {
+                if (event.start === conflictedEvent[0] && event.end === conflictedEvent[1]) {
+                    event.conflicted = true
+                    return;
+                }
+            })
+            if (event.conflicted) {
+                event.color = "yellow"
+            } else {
+                event.color = "blue"
+            }
+        }
+    );
+    return events
 }
+
 export const App: React.FC = () => {
     const myEvents = React.useMemo<MbscCalendarEvent[]>(() => {
         return [{
@@ -154,7 +188,7 @@ export const App: React.FC = () => {
             {
                 start: '2021-09-20T00:00',
                 end: '2021-09-23T00:00',
-                allDay:true
+                allDay: true
             }
         ]
     }, []);
@@ -180,7 +214,9 @@ export const App: React.FC = () => {
             data={myEvents}
             resources={myResources}
             invalid={myInvalid}
-            onEventCreateFailed={()=>{toast({message:"RANGE MAINT"})}}
+            onEventCreateFailed={() => {
+                toast({message: "RANGE MAINT"})
+            }}
         />
     );
 }
